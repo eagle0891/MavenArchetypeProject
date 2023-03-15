be72e48b6a9220cb72ac74c5e66870926a6e881e
Ugo Agwo <uagwo@wundermancommerce.com>
2023-03-15 00:33:06 +0000

    UA - Added helper files

--- SecondMavenAchetypeProject/src/test/java/Framework/BaseClass.java
+++ SecondMavenAchetypeProject/src/test/java/Framework/BaseClass.java
@@ -1,6 +1,5 @@
 package Framework;
 
-import io.cucumber.messages.types.Product;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.Getter;
 import org.junit.After;
@@ -12,24 +11,23 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertTrue;
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
-public class BaseClass {
+public class BaseClass extends WaitHelper {
     public static void main(String args[]) {
     }
 
+//    public BaseClass (WaitHelper waitHelper){
+//        this.waitHelper = waitHelper;
+//    }
     private WebDriver driver;
     protected static final Logger LOG = LoggerFactory.getLogger(BaseClass.class);
     @Getter
@@ -37,6 +35,7 @@ public class BaseClass {
     @Getter
     protected WebDriver webDriver;
 
+//    private WaitHelper waitHelper;
     public void openBrowser(String browser) throws MalformedURLException {
         switch (browser) {
             case "chrome" -> {
@@ -97,65 +96,20 @@ public class BaseClass {
     public void enterSearchTerm(String string){
         driver.findElement(By.cssSelector("#searchTerm")).sendKeys(string, Keys.RETURN);
     }
-
-    public void clickEnter(String string) {
-        driver.findElement(By.cssSelector(string)).sendKeys(Keys.RETURN);
-    }
-
-    public WebElement waitForExpectedElement(final By by, Duration timeout) {
-        WebDriverWait wait = new WebDriverWait(getWebDriver(), timeout);
-        try {
-            return wait.until(visibilityOfElementLocated(by));
-        } catch (NoSuchElementException | TimeoutException e) {
-            LOG.info(e.getMessage());
-            return null;
-        } catch (StaleElementReferenceException e) {
-            LOG.info(e.getMessage());
-            return wait.until(visibilityOfElementLocated(by));
-        }
-    }
-
-    private ExpectedCondition<WebElement> visibilityOfElementLocated(final By by) throws NoSuchElementException {
-        return driver -> {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                LOG.error("Error Message : " + e.getMessage());
-            }
-            WebElement element = getWebDriver().findElement(by);
-            return element.isDisplayed() ? element : null;
-        };
-    }
-
-    public void isElementDisplayed(By bySelector) {
-        boolean expectedElementDisplayed = false;
-        try {
-            //highlightElement(bySelector,1);
-            expectedElementDisplayed = waitForExpectedElement(bySelector, Duration.ofMillis(50)).isDisplayed();
-        } catch (Exception e) {
-            LOG.info("\nPresence of Element : " + expectedElementDisplayed);
-        }
-
-        assertTrue("\n\n ******** ERROR *********** \n" +
-                        "\n EXPECTED ELEMENT NOT DISPLAYED!! " +
-                        "\n Element NOT Found : " + bySelector +"\n",
-                expectedElementDisplayed);
-    }
-
     public void collectProducts(){
-          isElementDisplayed(By.cssSelector(".styles__ProductList-sc-1rzb1sn-1 .styles__LazyHydrateCard-sc-1rzb1sn-0 .ProductCardstyles__Title-h52kot-12"));
-//        List<WebElement> products = driver.findElements(By.cssSelector(".styles__ProductList-sc-1rzb1sn-1 .styles__LazyHydrateCard-sc-1rzb1sn-0"));
-//        System.out.println("Number of products is: " + products.size());
-//        for (WebElement product : products) {
-//            WebElement link = product.findElement(By.cssSelector(".ProductCardstyles__Title-h52kot-12"));
-//            WebElement button = product.findElement(By.cssSelector(".Buttonstyles__Button-sc-42scm2-2"));
-//
-//            String linkTextUrl = link.getAttribute("href");
-//            String buttonText = button.getText();
-//
-//            System.out.println(linkTextUrl);
-//            System.out.println(buttonText);
-//        }
+        isElementDisplayed(By.cssSelector(".styles__ProductList-sc-1rzb1sn-1 .styles__LazyHydrateCard-sc-1rzb1sn-0 .ProductCardstyles__Title-h52kot-12"));
+        List<WebElement> products = driver.findElements(By.cssSelector(".styles__ProductList-sc-1rzb1sn-1 .styles__LazyHydrateCard-sc-1rzb1sn-0"));
+        System.out.println("Number of products is: " + products.size());
+        for (WebElement product : products) {
+            WebElement link = product.findElement(By.cssSelector(".ProductCardstyles__Title-h52kot-12"));
+            WebElement button = product.findElement(By.cssSelector(".Buttonstyles__Button-sc-42scm2-2"));
+
+            String linkTextUrl = link.getAttribute("href");
+            String buttonText = button.getText();
+
+            System.out.println(linkTextUrl);
+            System.out.println(buttonText);
+        }
     }
 
     @After
