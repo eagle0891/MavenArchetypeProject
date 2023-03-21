21399b0cb534855cba16327e344270f32fc124ea
Ugo Agwo <upa01@hotmail.co.uk>
2023-03-21 18:30:51 +0000

    Gql scenarios with multiple parameters

--- SecondMavenAchetypeProject/src/test/java/GraphQLRestAssured/FirstTest.java
+++ SecondMavenAchetypeProject/src/test/java/GraphQLRestAssured/FirstTest.java
@@ -13,10 +13,12 @@ import static org.hamcrest.CoreMatchers.equalTo;
 @RunWith(Parameterized.class)
 public class FirstTest {
 
-    private String limit;
+    private final String limit;
+    private final String albumName;
 
-    public FirstTest(String limit) {
+    public FirstTest(String limit, String albumName) {
         this.limit = limit;
+        this.albumName = albumName;
     }
 
     @Test
@@ -28,15 +30,14 @@ public class FirstTest {
         String uri = "/.netlify/functions/index";
 
         given().log().all()
-            .contentType("application/json")
-            .body(query)
-                .when().log().all()
-                    .post(uri)
-                        .then().log().all()
-                            .assertThat()
-                                .statusCode(200)
-                                    .and()
-                                        .body("data.allFilms.films[0].title", equalTo("A New Hope"));
+                .contentType("application/json")
+                .body(query)
+                    .when().log().all()
+                        .post(uri)
+                            .then().log().all()
+                                .assertThat()
+                                    .statusCode(200)
+                                    .body("data.allFilms.films[0].title", equalTo("A New Hope"));
     }
 
     @Test
@@ -49,19 +50,22 @@ public class FirstTest {
                 .contentType("application/json")
                 .header("x-hasura-admin-secret","3m1kpYOAi6QkJsjCC1qpzHe0KTd1cDVffdlkqKq3DMrFHnpXxAAtpMNym7ZNHzKk")
                 .body(query)
-                .when().log().all()
-                .post(uri)
-                .then().log().all()
-                .assertThat()
-                .statusCode(200)
-                .body("data.Album.Tracks[0].TrackId[0]", equalTo(1))
-                .body("data.Album.Tracks[0].Name[0]", equalTo("For Those About To Rock (We Salute You)"));
+                    .when().log().all()
+                        .post(uri)
+                            .then().log().all()
+                                .assertThat()
+                                    .statusCode(200)
+                                    .body("data.Album.Tracks[0].TrackId[0]", equalTo(1))
+                                    .body("data.Album.Tracks[0].Name[0]", equalTo("For Those About To Rock (We Salute You)"));
 
     }
 
     @Parameterized.Parameters
     public static Collection<Object[]> getQueryData(){
-        return Arrays.asList(new Object[][]{{"3"}});
+        return Arrays.asList(new Object[][]{
+                {"3", "Restless and Wild"},
+                {"2", "Restless and Wild"},
+                {"4", "Restless and Wild"}});
     }
 
     @Test
@@ -78,13 +82,31 @@ public class FirstTest {
                 .contentType("application/json")
                 .header("x-hasura-admin-secret","3m1kpYOAi6QkJsjCC1qpzHe0KTd1cDVffdlkqKq3DMrFHnpXxAAtpMNym7ZNHzKk")
                 .body(query)
-                .when().log().all()
-                .post(uri)
-                .then().log().all()
-                .assertThat()
-                .statusCode(200)
-                .body("data.Album.Tracks[0].TrackId[0]", equalTo(1))
-                .body("data.Album.Tracks[0].Name[0]", equalTo("For Those About To Rock (We Salute You)"));
+                    .when().log().all()
+                        .post(uri)
+                            .then().log().all()
+                                .assertThat()
+                                    .statusCode(200)
+                                        .body("data.Album.Tracks[0].TrackId[0]", equalTo(1))
+                                        .body("data.Album.Tracks[0].Name[0]", equalTo("For Those About To Rock (We Salute You)"));
+
+    }
+
+    @Test
+    public void hasuraTestWithMultipleDataParams(){
+        RestAssured.baseURI = "https://great-ladybug-56.hasura.app";
+        String query = "{\"query\":\"query fetchAlbum {\\n  Album(limit: " + limit + ") {\\n    AlbumId\\n    Title\\n    Tracks(where: {Album: {Title: {_eq: \\\"" + albumName + "\\\"}}}, limit: 3) {\\n      TrackId\\n      Name\\n      Album {\\n        Title\\n      }\\n    }\\n  }\\n}\\n\",\"variables\":null,\"operationName\":\"fetchAlbum\"}";
+        String uri = "/v1/graphql";
+
+        given().log().all()
+                .contentType("application/json")
+                .header("x-hasura-admin-secret","3m1kpYOAi6QkJsjCC1qpzHe0KTd1cDVffdlkqKq3DMrFHnpXxAAtpMNym7ZNHzKk")
+                .body(query)
+                    .when().log().all()
+                        .post(uri)
+                            .then().log().all()
+                                .assertThat()
+                                    .statusCode(200);
 
     }
 }
