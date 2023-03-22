963571fd8d1a9003e91296274afeb0678e41ec8b
Ugo Agwo <upa01@hotmail.co.uk>
2023-03-22 18:32:20 +0000

    array examples

--- SecondMavenAchetypeProject/src/test/java/Concepts/ArrayListDemo.java
+++ SecondMavenAchetypeProject/src/test/java/Concepts/ArrayListDemo.java
@@ -5,12 +5,16 @@ import org.codehaus.groovy.runtime.ArrayUtil;
 import javax.lang.model.type.ArrayType;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Objects;
 
 public class ArrayListDemo {
 
     public static void main(String[] args){
-        dynamicArrayListExample();
+        arrayListIteration();
+        arrayListContainingCollection();
+        arrayListMethods();
     }
 
     //static array
@@ -66,13 +70,89 @@ public class ArrayListDemo {
 
         //Typical for loop
         for (int i=0; i<list.size(); i++) {
-            System.out.println(list.get(i));
+            System.out.println("Typical for loop: " + list.get(i));
         }
+
+        System.out.println("-----------------");
 
         //Enhanced for loop equivalent
         for (String s : list) {
-            System.out.println(s);
+            System.out.println("Enhanced for loop: " + s);
+        }
+
+        System.out.println("-----------------");
+
+        //streams with lambda
+        list.stream().forEach(ele -> System.out.println("Streams with lambda: " + ele));
+
+        System.out.println("-----------------");
+
+        Iterator<String> it = list.iterator();
+        while (it.hasNext()) {
+            System.out.println("Iterator: " + it.next());
         }
     }
 
+    public static void arrayListContainingCollection(){
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(10, 20, 30, 40, 50));
+        System.out.println(numbers.size() + " numbers in the list: " + numbers);
+
+        ArrayList<String> footballTeams = new ArrayList<>(Arrays.asList("Arsenal", "Liverpool", "Everton", "Newcastle", "Aston Villa"));
+        System.out.println(footballTeams.size() + " football teams in the list: " + footballTeams);
+    }
+
+    public static void arrayListMethods(){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Tom");
+        list.add("Mohammed");
+        list.add("jake");
+        list.add("Daniel");
+        list.add("Ade");
+        list.add("Mr Schmidt");
+        System.out.println(list);
+
+        ArrayList<String> vehicles = new ArrayList<>();
+        vehicles.add("Bus");
+        vehicles.add("car");
+        vehicles.add("bike");
+        vehicles.add("Skateboard");
+        vehicles.add("unicycle");
+        vehicles.add("pogo stick");
+        System.out.println(vehicles);
+
+        list.addAll(vehicles);
+        System.out.println(list);
+
+        list.addAll(2, vehicles); //ad "vehicles" list into the 2nd index of the "list"
+        System.out.println(list);
+
+        ArrayList<String> clonedList = (ArrayList<String>)list.clone();
+        System.out.println("Cloned list is: " + clonedList);
+        System.out.println("List contains pogo stick: " + list.contains("pogo stick"));
+        System.out.println("List contains surfboard: " + list.contains("surfboard"));
+        System.out.println("List contains jack: " + list.contains("jack"));
+        System.out.println("Tom is the first in the list: " + (list.indexOf("Tom")==0));
+        System.out.println("Mohammed is the first in the list: " + (list.indexOf("Mohammed")==0));
+        System.out.println("Mohammed is first or second in the list: " + (list.indexOf("Mohammed")<2));
+        System.out.println("What is the last index of the list? " + (list.lastIndexOf("pogo stick")));
+        System.out.println("Which index of the list is TestName in? " + (list.lastIndexOf("TestName"))); //NOTE: this will return -1 (and not error)
+
+        list.remove("pogo stick"); //removes first found in the list
+        System.out.println(list);
+        list.remove("pogo stick");
+        System.out.println(list);
+
+        list.removeIf(item -> item=="Tom");
+        System.out.println(list);
+
+        ArrayList<Integer> digits = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        System.out.println(digits);
+        digits.removeIf(num -> num%2 == 0); //"->" is a lambda function //remove all even numbers
+        System.out.println(digits);
+
+//        ArrayList<Integer> figures = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+//        System.out.println(figures);
+//        digits.removeIf(num -> num%2+1 == 0); //"->" is a lambda function
+//        System.out.println(figures);
+    }
 }
